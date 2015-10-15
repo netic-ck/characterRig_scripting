06c604524ae3d20a94e0f1cfedcf2235090012cb
Ernesto Ruiz Velasco <ernesto@ruizvelasco.com>
2015-10-15 09:56:10 -0700

    init

---
+++ setupRigUI.py
@@ -0,0 +1,43 @@
+__author__ = 'Ernesto Ruiz Velasco'
+__script__ = 'setupRigUI.py'
+
+'''
+This script contains procedures that crate a setup UI for all the scripts that create a biped rig.
+Based in the book "MEL Scripting a Character Rig in Maya 2008"
+'''
+
+#    checkCharName()
+#    setCharName()
+#    setNamespace()
+#    setupRigUI()
+
+
+def checkCharName():
+    '''
+    This function finds the name of the character
+    '''
+    pass
+
+
+def setCharName():
+    '''
+    This function sets the name of the character
+    '''
+    pass
+
+
+def setNamespace():
+    '''
+    This function sets the a namespace for the character
+    '''
+    pass
+
+
+def setupRigUI():
+    '''
+    This function launches the SetupRigUI
+    '''
+    pass
+
+
+
